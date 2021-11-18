5c1c21c745dbe25a9d8b971c6c58afceee5995b4
SumZbrod <SumZbrod@gmail.com>
2021-11-18 21:14:51 +0300

    generate_deck

--- video_spliter.py
+++ video_spliter.py
@@ -3,6 +3,11 @@ import moviepy.editor
 import os
 from config import *
 import shutil
+import genanki
+import random
+import pydub 
+import numpy as np
+import pydub 
 
 def total_seconds(str_time: str) -> float:
     """
@@ -28,9 +33,20 @@ def made_script_list(scr):
                 text = scr[1+i]
                 if '\u202a' in text:
                     text = text[1:]
-                    text.replace('\u202a', '')
+                    text = text.replace('\u202a', '')
                     res_list.append((tuple(map(total_seconds, block.split(' --> '))), text))
     return res_list
+
+def write(f, x, sr=48000, normalized=False):
+    """numpy array to MP3"""
+    x = 30000*x
+    channels = 2 if (x.ndim == 2 and x.shape[1] == 2) else 1
+    if normalized:  # normalized array - each item should be a float in [-1, 1)
+        y = np.int16(x * 2 ** 15)
+    else:
+        y = np.int16(x)
+    song = pydub.AudioSegment(y.tobytes(), frame_rate=sr, sample_width=2, channels=channels)
+    song.export(f, format="mp3", bitrate="320k")
 
 def split_video(file_name):
     file_path = path_to_dir+file_name
@@ -46,16 +62,34 @@ def split_video(file_name):
             for i, block in enumerate(script):
                 delta_time, text = block
                 sub_video = video.subclip(*delta_time)
-                sub_video.write_videofile(f'content/video/{text}.mp4')
-                sub_video.audio.write_audiofile(f'content/audio/{text}.mp3')
+                # sub_video.write_videofile(f'{video_path}{text}.mp4')
+                write(f'{audio_path}{text}.mp3', sub_video.audio.to_soundarray())
                 if i > 3:
                     break
+
+def generate_deck(audio_list, deck_name):
+    anki_deck = genanki.Deck(random.randrange(1 << 30, 1 << 31), deck_name)
+    for name in audio_list:
+        print(name)
+        note = genanki.Note(model=anki_model, fields=[f'[sound:{name}]', name.split('.')[0]])
+        anki_deck.add_note(note)
+    my_package = genanki.Package(anki_deck)
+    my_package.media_files = list(map(lambda x: audio_path+x, audio_list))
+    my_package.write_to_file(f'{deck_name}.apkg')
+    # .write_to_file(f'{deck_name}.apkg')
+    # genanki.Package(my_package).write_to_file(f'{deck_name}.apkg')
 
 if __name__ == '__main__':
 
     file_list = list(filter(lambda x: os.path.isfile(path_to_dir+x), os.listdir(path_to_dir)))
 
     for file_name in file_list:
+        break
         split_video(file_name)
 
-    # audio_list = list(filter(lambda x: x[-4:]=='.mp4', os.listdir('content/audio')))
+    audio_list = list(filter(lambda x: x[-4:]=='.mp3', os.listdir(audio_path)))
+    # audio_list = list(filter(lambda x: x[-4:]=='.mp3', os.listdir()))
+    # video_list = list(filter(lambda x: x[-4:]=='.mp4', os.listdir(audio_path)))
+
+    generate_deck(audio_list, 'test_')
+    
